809a478e986752a6f3697d631f209acd429587af
eugeneLuck <eugeneLuck@gmail.com>
2023-08-04 19:37:07 +0300

    Show countries

---
+++ AppMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.PerformanceData;
+using System.Windows.Forms;
+
+namespace Climbs
+{
+    public partial class StartForm
+    {
+        MenuStrip menu;
+        ToolStripMenuItem dataMenu;
+        ToolStripMenuItem editMenu;
+
+        ToolStripMenuItem countryData;
+        ToolStripMenuItem mountainData;
+
+        ToolStripMenuItem insertCommand;
+        ToolStripMenuItem deleteCommand;
+        ToolStripMenuItem updateCommand;
+
+        void CreateMenu()
+        {
+            menu = new MenuStrip();
+            Controls.Add(menu);
+
+            dataMenu = new ToolStripMenuItem("&Data");
+            editMenu = new ToolStripMenuItem("&Edit");
+            menu.Items.AddRange(new ToolStripMenuItem[] 
+                { dataMenu, editMenu });
+
+            countryData = new ToolStripMenuItem("&Countries");
+            countryData.Click += (s, e) => ShowCountries();
+            mountainData = new ToolStripMenuItem("&Mountains");
+            dataMenu.DropDownItems.AddRange(new ToolStripMenuItem[]
+                { countryData, mountainData });
+
+            insertCommand = new ToolStripMenuItem("&Insert");
+            deleteCommand = new ToolStripMenuItem("&Delete");
+            updateCommand = new ToolStripMenuItem("&Update");
+            editMenu.DropDownItems.AddRange(new ToolStripMenuItem[]
+                { insertCommand, deleteCommand, updateCommand });
+        }
+    }
+}
--- StartForm.cs
+++ StartForm.cs
@@ -1,25 +1,58 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
 using static Climbs.Properties.Resources;
 
 namespace Climbs
 {
     public partial class StartForm : Form
     {
+
+        TabControl tabControl;
+        DataGridView grid;
+
+        string connectionString;
+        SqlConnection connection;
+
         public StartForm()
         {
             InitializeComponent();
             MakeControls();
             SetForm();
+
+            connectionString =
+                @"Data Source=(localdb)\MSSQLLocalDB;"
+                + "Initial Catalog=Climbers";
+            connection = new SqlConnection(connectionString);
         }
 
         void MakeControls()
         {
-
+            CreateMenu();
+            tabControl = new TabControl();
+            tabControl.Dock = DockStyle.Fill;
+            Controls.Add(tabControl);
         }
 
         void SetForm()
         {
             (Text, Icon) = (AppName, AppIcon);
             StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        void ShowCountries()
+        {
+            string queryText = "Select * from Country";
+            SqlDataAdapter adapter = new SqlDataAdapter(queryText, connection);
+            new SqlCommandBuilder(adapter);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            TabPage tab = new TabPage("Countries");
+            tabControl.Controls.Add(tab);
+            grid = new DataGridView();
+            grid.Dock = DockStyle.Fill;
+            tab.Controls.Add(grid);
+            grid.DataSource = dataTable;
         }
     }
 }
